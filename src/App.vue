49830478314d50fdabb7b4537fdaa03dcd9aad3b
yangang2019 <cd_yangang@163.com>
2020-03-22 13:17:24 +0800

    '添加顶部和底部模块'

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
+    <div class="app-container">
+        <!--顶部模块-->
+        <mt-header fixed title="固定在顶部"></mt-header>
+        <!--中间模块-->
+
+        <!--底部模块-->
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item mui-active" href="#tabbar">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-chat">
+                <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+                <span class="mui-tab-label">消息</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-contact">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">通讯录</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-map">
+                <span class="mui-icon mui-icon-gear"></span>
+                <span class="mui-tab-label">设置</span>
+            </a>
+        </nav>
         <h1>这是App组件</h1>
     </div>
 </template>
@@ -11,5 +34,11 @@
 </script>
 
 <style scoped lang="less">
-
+    .app-container {
+        padding-top: 40px;
+        h1 {
+            margin: 0;
+            padding: 0;
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 //入口文件
-console.log(123);
+import Vue from 'vue';
+import { Header } from 'mint-ui';
+
+import app from './App.vue';
+import '../lib/mui/css/mui.min.css';
+
+Vue.component(Header.name, Header);
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app)
+})
